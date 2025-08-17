f49d301ed61f8ce2e753cd707d43d377084bb09e
Clara Gonçalves Ribeiro <85358746+ClaraGRibeiro@users.noreply.github.com>
2025-08-17 10:28:04 -0300

    Fixing the nocoverbook image bug

--- src/components/gridBooks.tsx
+++ src/components/gridBooks.tsx
@@ -26,14 +26,14 @@ const GridBooks = ({ books, handles, pinReadings }: GridBooksProps) => {
     const [clickedBook, setClickedBook] = useState<Book | null>(null)
     return (
         <>
-            <div className='grid grid-cols-3 md:grid-cols-6 gap-8 justify-items-center'>
+            <div className='grid grid-cols-2 md:grid-cols-6 gap-8 justify-items-center'>
                 {sortedBooks.map((b) => (
                     <div key={b.id} className={(b.status === 'Reading' ? 'shadow-[0_0_10px_#f4d177,0_0_15px_#f4d177,0_0_35px_#f4d177]' : "") + ' relative hover:scale-110 duration-200 cursor-pointer group'} onClick={() => setClickedBook(b)}>
-                        <img className='max-h-48' src={b.image || 'https://cdn.creazilla.com/cliparts/3164704/book-clipart-md.png'} alt={b.title} title={b.status + " ~ " + b.title} />
+                        <img className='max-h-48' src={b.image || 'nocoverbook.png'} alt={b.title} title={b.status + " ~ " + b.title} />
                         <BookmarkFilledIcon className={
                             (b.status == 'Read' ? 'text-[#02A9F4]' :
                                 b.status == 'Unread' ? 'text-[#FF6666]' :
-                                    'text-[#FBAC0F]') + ' absolute top-0 right-0 p-0 m-0 opacity-40 duration-200 h-8 w-8 group-hover:opacity-100'} />
+                                    'text-[#FBAC0F]') + ' absolute top-0 right-0 p-0 m-0 opacity-60 duration-200 h-8 w-8 group-hover:opacity-100'} />
                         {!b.image && <span className='absolute transform top-1/2 -translate-y-1/2 overflow-hidden text-center p-6 break-words w-[90%] max-h-[90%]'>{b.title}</span>}
                     </div>
                 ))}
