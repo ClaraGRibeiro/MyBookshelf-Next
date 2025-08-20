0c35d94aae5bf676b15aa03d498cc29ef44b4c1a
Clara Gonçalves Ribeiro <85358746+ClaraGRibeiro@users.noreply.github.com>
2025-08-20 09:21:29 -0300

    Adding bar charts

--- src/components/bookshelf.tsx
+++ src/components/bookshelf.tsx
@@ -27,7 +27,11 @@ const Bookshelf = ({ books, handles }: BookShelfProps) => {
   const monthNow = new Date().getMonth() + 1
   const readingsY = books.filter((b) => b.readDate?.split('/')[2] === yearNow.toString()).length
   const readingsM = books.filter((b) => b.readDate?.split('/')[2] === yearNow.toString() && Number(b.readDate.split('/')[1]) === monthNow).length
-
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  const booksByMonth = months.map((m, i) => { return { name: m, value: books.filter((b) => b.readDate?.split('/')[2] === yearNow.toString() && Number(b.readDate.split('/')[1]) === i + 1).length } })
+  const years = Array.from(new Set(books.filter(b => b.readDate).map((b) => Number(b.readDate?.split('/')[2])))).sort((x, y) => x - y)
+  const booksByYear = years.map((y, i) => {return { name: y.toString(), value: books.filter((b) => Number(b.readDate?.split('/')[2]) === y).length } })
+  
   return (
     <main className='p-12 text-slate-800'>
       <div className='fixed bottom-8 right-3 flex flex-col gap-2 items-center justify-center'>
@@ -75,20 +79,22 @@ const Bookshelf = ({ books, handles }: BookShelfProps) => {
       </div>
 
       <h1 className='text-center text-3xl mb-6 font-bold'>My BookShelf</h1>
-      <div className='flex flex-wrap gap-12 justify-center items-center mb-2'>
-        <Chart value={numBooksRead} nameValue='Readings' total={books.length} nameTotal='Remaining' label='Readings' colors={['#006AB3', '#cad5e2']} />
-        <Chart value={readingsY} nameValue='In this year' total={numBooksRead} nameTotal='Readings in others years' label={'In ' + yearNow} colors={['#E63431', '#cad5e2']} />
-        <Chart value={readingsM} nameValue='In this month' total={readingsY} nameTotal='Readings in others months of this years' label={'In ' + new Date().toLocaleString('en-US', { month: 'long' })} colors={['#FBAC0F', '#cad5e2']} />
-      </div>
-      <p className="text-center font-bold mb-12 text-xl">
-        <span className="bg-gradient-to-r from-[#006AB3] via-[#E63431] to-[#FBAC0F] bg-clip-text text-transparent">R$ {totalSpent}</span>
-      </p>
 
       {grid ? (
         <GridBooks books={books} handles={handles} pinReadings={pinReadings} />
       ) : (
         <TableBook books={books} handles={handles} pinReadings={pinReadings} />
       )}
+      <div className='mt-12 md:mt-16 flex flex-col gap-4 items-center justify-center'>
+        <p className="text-center font-bold text-xl"><span className="bg-gradient-to-r from-[#006AB3] via-[#E63431] to-[#FBAC0F] bg-clip-text text-transparent">R$ {totalSpent} total</span></p>
+        <div className='flex flex-wrap gap-4 md:gap-12 justify-center items-center mb-2'>
+          <Chart type='pie' value={numBooksRead} nameValue='Readings' total={books.length} nameTotal='Remaining' label='Readings' colors={['#006AB3', '#cad5e2']} />
+          <Chart type='pie' value={readingsY} nameValue='In this year' total={numBooksRead} nameTotal='Readings in others years' label={'In ' + yearNow} colors={['#E63431', '#cad5e2']} />
+          <Chart type='pie' value={readingsM} nameValue='In this month' total={readingsY} nameTotal='Readings in others months of this years' label={'In ' + new Date().toLocaleString('en-US', { month: 'long' })} colors={['#FBAC0F', '#cad5e2']} />
+        </div>
+        <Chart type='bar' data={booksByMonth} label='Books per Month (2025)' colors={['#E63431']} />
+        <Chart type='bar' data={booksByYear} label='Books per Year' colors={['#E63431']} />
+      </div>
     </main>
   )
 }
--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -1,46 +1,68 @@
 'use client'
-import { PieChart, Pie, Cell, Tooltip } from 'recharts'
+import { PieChart, Pie, Cell, Tooltip, BarChart, Bar, XAxis, YAxis, CartesianGrid, Legend } from 'recharts'
 
 type ChartProps = {
-  value: number
-  nameValue: string
-  total: number
-  nameTotal: string
-  label: string
-  colors?: [string, string]
+  type: 'pie' | 'bar'
+  data?: { name: string; value: number }[]
+  value?: number
+  nameValue?: string
+  total?: number
+  nameTotal?: string
+  label?: string
+  colors?: string[]
 }
 
-const Chart = ({ value, nameValue, total, nameTotal, label, colors = ['#cad5e2', '#cad5e2'] }: ChartProps) => {
-  const data = [
-    { name: nameValue, value },
-    { name: nameTotal, value: total - value },
-  ]
+const Chart = ({ type, value, nameValue, total, nameTotal, label, data, colors = ['#cad5e2', '#cad5e2'] }: ChartProps) => {
+  if (type === 'pie') {
+    const pieData = [
+      { name: nameValue, value },
+      { name: nameTotal, value: value && total ? (total - value) : 0 },
+    ]
 
-  return (
-    <div className='flex flex-col items-center text-sm'>
-      <PieChart width={80} height={80}>
-        <Pie
-          data={data}
-          cx='50%'
-          cy='50%'
-          innerRadius={18}
-          outerRadius={30}
-          startAngle={90}
-          endAngle={-270}
-          dataKey='value'
-        >
-          {data.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
-          ))}
-        </Pie>
-        <Tooltip />
-      </PieChart>
-      <span>{label}</span>
-      <span className='text-xs text-slate-800'>
-        {value}/{total} ({((value * 100) / total).toFixed(0)}%)
-      </span>
-    </div>
-  )
+    return (
+      <div className='flex flex-col items-center text-sm'>
+        <PieChart width={80} height={80}>
+          <Pie
+            data={pieData}
+            cx='50%'
+            cy='50%'
+            innerRadius={18}
+            outerRadius={30}
+            startAngle={90}
+            endAngle={-270}
+            dataKey='value'
+          >
+            {pieData.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+            ))}
+          </Pie>
+          <Tooltip />
+        </PieChart>
+        <span>{label}</span>
+        <span className='text-xs text-slate-800'>
+          {value}/{total} ({value && total && (((value * 100) / total).toFixed(0))}%)
+        </span>
+      </div>
+    )
+  }
+  
+  if (type === 'bar' && data) {
+    return (
+      <div className='flex flex-col items-center text-sm'>
+        <BarChart width={420} height={200} data={data} margin={{ top: 0, right: 40, left: 0, bottom: 0 }} barCategoryGap={2}>
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='name' />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey='value' fill={colors[0]} />
+        </BarChart>
+        <span>{label}</span>
+      </div>
+    )
+  }
+
+  return null
 }
 
 export default Chart
